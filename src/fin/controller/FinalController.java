51d89a6b3e8ca99f11c92d1945be8ef9a93da2e0
hkno9012 <43076781+hkno9012@users.noreply.github.com>
2019-04-19 15:05:15 -0600

    Copied controller code from GitHub

--- src/fin/controller/FinalController.java
+++ src/fin/controller/FinalController.java
@@ -1,5 +1,136 @@
 package fin.controller;
 
-public class FinalController {
+import javax.swing.JOptionPane;
 
+import fin.model.*;
+import fin.model.Character;
+import fin.view.*;
+
+public class FinalController
+{
+	private FinalFrame appFrame;
+	private GamePanel appPanel;
+	
+	private Character player;
+	private Character enemy;
+	
+	public FinalController()
+	{
+		appFrame = new FinalFrame(this);
+	}
+	
+	public FinalFrame getFrame()
+	{
+		return appFrame;
+	}
+	
+	public void start()
+	{
+		/*if(appPanel.getPlayerHPValue() < 1)
+		{
+			appPanel.changeLoseLabel();
+		}
+		if(appPanel.getEnemyHPValue() < 1)
+		{
+			appPanel.changeWinLabel();
+		}*/
+	}
+	
+	public void play()
+	{
+		appFrame.replaceScreen("GAME");
+		makeEnemy();
+	}
+	
+	public void makeCharacter(int type)
+	{
+		if(type == 1)
+		{
+			player = new Warrior();
+		}
+		if(type == 2)
+		{
+			player = new Wizard();
+		}
+		if(type == 3)
+		{
+			player = new Thief();
+		}
+		if(type == 4)
+		{
+			player = new Archer();
+		}
+		if(type == 5)
+		{
+			enemy = new Warrior();
+		}
+		if(type == 6)
+		{
+			enemy = new Wizard();
+		}
+		if(type == 7)
+		{
+			enemy = new Thief();
+		}
+		if(type == 8)
+		{
+			enemy = new Archer();
+		}
+	}
+	
+	public void makeEnemy()
+	{
+		/*4 is how many numbers there are, and 5 is the first of those 4 numbers*/
+		makeCharacter((int)(Math.random() * 4) + 5);
+	}
+	
+	public void playerFight()
+	{
+		int damage = calculateDamage(10, 6);
+		enemy.setHealth(enemy.getHealth() - damage);
+		/*appPanel.setEnemyHPValue(appPanel.getEnemyHPValue() - damage);*/
+	}
+	
+	public void enemyFight()
+	{
+		int damage = calculateDamage(10, 6);
+		player.setHealth(player.getHealth() - damage);
+		/*appPanel.setPlayerHPValue(appPanel.getPlayerHPValue() - damage);*/
+	}
+	
+	public int defend()
+	{
+		int extraDefense = 5;
+		return extraDefense;
+	}
+	
+	/*public int potion()
+	{
+		
+	}*/
+
+	public int calculateDamage(int attack, int defense)
+	{
+		/*A number between 1 and 20 is generated*/
+		int criticalChance = ((int)(Math.random() * 20) + 1);
+		/*The damage will be between below 2 and above 2 the character's attack stat*/
+		int damage = ((attack - defense) + ((int)(Math.random() * 5) - 2));
+		if(criticalChance == 20)
+		{
+			damage = damage * 2;
+		}
+		return damage;
+	}
+	
+	/*public int getPlayerHealth()
+	{
+		int health = player.getHealth();
+		return health;
+	}
+	
+	public int getEnemyHealth()
+	{
+		int health = enemy.getHealth();
+		return health;
+	}*/
 }
--- src/fin/controller/FinalRunner.java
+++ src/fin/controller/FinalRunner.java
@@ -1,5 +1,10 @@
 package fin.controller;
 
-public class FinalRunner {
-
+public class FinalRunner
+{
+	public static void main(String [] args)
+	{
+		FinalController app = new FinalController();
+		app.start();
+	}
 }
